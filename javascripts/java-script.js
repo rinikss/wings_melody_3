0dc80f34b6dc8187b793d8d9dd689f92635355f5
рина <arinaskamina@gmail.com>
2026-03-02 08:29:45 +0300

    commmmmmm

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -1,15 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-
-    let myFunction = function() { // Тело функции
-        let integriruemtext = document.getElementById('integriruemtext');
-
-        integriruemtext.textContent = "Hi student!"; // Добавили текст в див
-
-        integriruemtext.style.fontFamily = 'CarlaSansLight'; // добавили стили к тексту 
-        integriruemtext.style.fontSize = "35px"; // добавили стили к тексту 
-    }; 
-    
-    myFunction(); // Вызов функции
-    
-    
-});
+document.addEventListener("DOMContentLoaded", function () {});
